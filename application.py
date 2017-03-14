db04eb1af583f3a42ef895716f81a17916cd6afd
donaldjones09 <donald.jones600@gmail.com>
2017-03-14 19:08:15 -0400

    SQL addition
    Added SQLAlchemy functionality

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from datetime import datetime
+from sqlalchemy import *
+
+db = create_engine('sqlite:///section2.db')
 
 app = Flask(__name__)
 
@@ -13,18 +16,14 @@ def index():
 def outdoor():
     return render_template("outdoor.html")
 
+@app.route("/outdoor/outdoorschedule")
+def outdoor_schedule():
+    meets = outdoorschedule.query.filter_by(year = current_year).first()
+    return render_template("outdoorschedule.html",rows = meets, current_year = current_year)
+
 @app.route("/indoor", methods=["GET", "POST"])
 def indoor():
     return render_template("indoor.html")
-
-@app.route("/xc", methods=["GET", "POST"])
-def xc():
-    return render_template("xc.html")
-
-@app.route("/outdoor/outdoorschedule")
-def outdoor_schedule():
-    #show all current_year meets (todo)
-    return render_template("outdoorschedule.html", current_year = current_year)
 
 @app.route("/indoor/indoorschedule")
 def indoor_schedule():
@@ -35,7 +34,14 @@ def indoor_schedule():
     #show all meets for indcurrent_year - indcurrent_year - 1 (todo)
     return render_template("indoorschedule.html", current_year = indcurrent_year)
 
+@app.route("/xc", methods=["GET", "POST"])
+def xc():
+    return render_template("xc.html")
+
 @app.route("/xc/xcschedule")
 def xc_schedule():
+    return render_template("xcschedule.html", current_year = current_year)
 
-    return render_template("xcschedule.html", current_year = xccurrent_year)
+@app.route("/meetinfo")
+def meetinfo():
+    return render_tempate("meetinfo.html")
